439415d1a0ed281ae44cfe5f68b81fb9084f4163
Soilus-664 <marcosadrian450@gmail.com>
2024-09-12 10:17:37 -0300

    criação do projeto junto com a finalização de tabs

---
+++ resources/views/tela-teste.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+    <title>Faz o L</title>
+</head>
+<body>
+    
+<h1 class="bg-black text-white p-4 rounded-xl ">Pai Aqui é Foda</h1>
+
+<button class="btn">Button</button>
+<button class="btn btn-neutral">Neutral</button>
+<button class="btn btn-primary">Primary</button>
+<button class="btn btn-secondary">Secondary</button>
+<button class="btn btn-accent">Accent</button>
+<button class="btn btn-ghost">Ghost</button>
+<button class="btn btn-link">Link</button>
+
+
+</body>
+</html>
